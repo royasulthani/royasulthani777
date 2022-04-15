0c1c5061e27433481fec1a8e3ced918532e28e2e
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-04-16 01:52:21 +0400

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10_aprel_practica
+{
+    class Group
+    {
+        public Student[] students = new Student[0];
+        public  int GroupNo { get; set; }
+        public void AddStudent(Student student)
+        {
+            Array.Resize(ref students, students.Length + 1);
+            students[students.Length - 1] = student;
+         }
+
+
+
+
+    }
+}
